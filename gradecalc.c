698b504645e4d1ad106cfe60cf433e053bbb34b6
borday <iwasdumbyforu@gmail.com>
2025-03-10 20:10:02 +0400

    Dəyərrər dosyasın yaratmax üçün lazım olan kod bitdi.

--- gradecalc.c
+++ gradecalc.c
@@ -9,30 +9,50 @@ void bir_işarə_qoş(char *yazı, char işarə){
 }
 
 void ayar_dosyası_yarat(){
-	int sıra, dərs_sayı;
-	char yığışma[128];
+	int sıra0, sıra1, dərs_sayı, sınaq_sayı, dəyər;
+	char dərsin_adı[64], yığışma[128];
 	FILE *dosya_göstərən;
 
-	dosya_göstərən = fopen("dəyərlər.bad", "w");
-
+	dosya_göstərən = fopen("dəyərlər.byd", "w");
 
 	printf("Dərslərin sayı: ");
 	scanf("%d", &dərs_sayı);
 
-	sıra = 0;
-	while(sıra < dərs_sayı){
-		printf("Dərs %d adı: ", sıra);
-		scanf("%s", &yığışma);
-		bir_işarə_qoş(yığışma, '\n');
+	sıra0 = 0;
+	while(sıra0 < dərs_sayı){
+		printf("Dərs %d adı: ", sıra0);
+		scanf("%s", &dərsin_adı);
+			
+		sprintf(yığışma, ">%s\n", dərsin_adı);
 		fputs(yığışma, dosya_göstərən);
 
-		yığışma[0] = 0;
-		sıra++;
+		printf("---\nDərs %d dəyəri: ", sıra0);
+		scanf("%d", &dəyər);
+		
+		sprintf(yığışma, "%d\n", dəyər);
+		fputs(yığışma, dosya_göstərən);
+
+		printf("---\nSınaqların sayı: ");
+		scanf("%d", &sınaq_sayı);
+
+		printf("---------------------\n");
+		sıra1 = 0;
+		while(sıra1 < sınaq_sayı){
+			printf("Sınaq %d dəyəri: ", sıra1);
+			scanf("%d", &dəyər);
+
+			sprintf(yığışma, "%d\n", dəyər);
+			fputs(yığışma, dosya_göstərən);
+			
+			sıra1++;
+		}
+		fputs("<\n", dosya_göstərən);
+		sıra0++;
+		printf("\n>==========================<\n");
 	}
 	fclose(dosya_göstərən);
 }
 
 int main(void){
-	ayar_dosyası_yarat();
 	return 0;
 }
